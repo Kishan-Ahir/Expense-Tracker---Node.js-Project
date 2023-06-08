e76da2a6dda5a1ab6df8479225d5d172114745a5
Kishan-Ahir <chandravadiyakishan77@gmail.com>
2023-06-08 17:25:10 +0530

    Login Page Backend

--- server.js
+++ server.js
@@ -4,68 +4,114 @@ const path = require("path");
 const bodyparser = require("body-parser");
 const cors = require("cors");
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("expenses","root","Chandravadiya@2003",{
-    host: "localhost",
-    dialect: "mysql"
+
+const sequelize = new Sequelize("expenses", "root", "Chandravadiya@2003", {
+  host: "localhost",
+  dialect: "mysql"
 });
 
-const User = sequelize.define("user",{
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement : true,
-        allowNull : false,
-        primaryKey: true
-    },
-    name : {
-        type : Sequelize.STRING,
-        allowNull : false
-    },
-    email : {
-        type : Sequelize.STRING,
-        allowNull : false
-    },
-    password : {
-        type : Sequelize.STRING,
-        allowNull : false
-    }
+// Define User model
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 });
 
+// Middleware
 app.use(cors());
-
 app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.post("/user/signup",async (req,res)=>{
+// Sign up route
+app.post("/user/signup", async (req, res) => {
+  try {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    let userexist;
-    await User.findAll({where: {email:email}})
-    .then(async (user)=>{
-        if(user[0])
-        {
-            userexist = true;
-            res.json(userexist)
-            
-        } else {
-            await User.create({
-                name:name,
-                email:email,
-                password:password
-            }).then(()=>{
-                userexist = false;
-                return res.json(userexist);
-            })
-        }
-    })
-})
+    let userExists = false;
 
-app.get("/",(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname,"Signup Screen Frontend.html"));
-})
+    function isStringNotValid(string) {
+      if (string.length !== 0) return false;
+      else return true;
+    }
 
-User.sync()
-.then(()=>{
-    console.log("Server is starting...");
-    app.listen(3000);
-})
+    if (
+      isStringNotValid(name) ||
+      isStringNotValid(email) ||
+      isStringNotValid(password)
+    ) {
+      if (isStringNotValid(name))
+        return res.status(500).json("enter correct name");
+      if (isStringNotValid(email))
+        return res.status(500).json("enter correct email");
+      if (isStringNotValid(password))
+        return res.status(500).json("enter correct password");
+    }
+
+    await User.findAll({ where: { email: email } }).then(async (users) => {
+      if (users[0]) {
+        userExists = true;
+        return res.status(400).json(userExists);
+      }
+      await User.create({
+        name: name,
+        email: email,
+        password: password
+      }).then(() => {
+        return res.status(201).redirect("/");
+      });
+    });
+  } catch (err) {
+    res.status(500).json("server error");
+  }
+});
+
+// Login page route
+app.get("/user/login", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "Login Screen Frontend.html"));
+});
+
+// Login check route
+app.post("/user/logincheck", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const users = await User.findAll({ where: { email: email } });
+
+    if (users[0]) {
+      if (users[0].password !== password) {
+        return res.status(403).json("Please enter correct password");
+      }
+      return res.status(200).json("Login Successfully.");
+    } else return res.status(404).json("User not found");
+  } catch (err) {
+    return res.status(404).json(err);
+  }
+});
+
+// Home page route
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "Signup Screen Frontend.html"));
+});
+
+// Sync User model and start server
+User.sync().then(() => {
+  console.log("Server is starting...");
+  app.listen(3000);
+});
